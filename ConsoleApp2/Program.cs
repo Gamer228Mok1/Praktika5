8fd297b9fee5e247f8c391ce8d5f73706f703768
Gamer228Mok1 <is25graschenkoda@artcollege.ru>
2024-02-24 15:46:26 +0300

    добваил получение числа

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите X");
+            double x = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
